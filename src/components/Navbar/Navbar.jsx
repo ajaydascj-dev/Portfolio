e2ca011b8c2acc9401c6308ab9f007d65535f737
Ajay Das <ajaydascj123@gmail.com>
2024-06-18 00:27:59 +0530

    Navbar Bug fixed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -27,14 +27,11 @@ const Navbar = () => {
         {toggle && (
           <motion.div
             whileInView={{ x: [300, 0] }}
-            transition={{ duration: 0.85, ease: "easeOut" }}
+            transition={{ duration: 0.1, ease: "easeOut" }}
             onClick={() => setToggle(false)}
           >
             {/* {<HiX onClick={() => setToggle(false)} /> } */}
-            <motion.ul 
-            whileInView={{ x: [300, 0] }}
-            transition={{ duration: 0.85, ease: "easeOut" }}
-            > 
+            <ul> 
             {links.map((link) => {
               return (
                 <li key={link} >
@@ -42,7 +39,7 @@ const Navbar = () => {
                 </li>
               );
             })}
-            </motion.ul>  
+            </ul>  
           </motion.div>
         )}
       </div>
